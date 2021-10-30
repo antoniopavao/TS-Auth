7d889c3d942be39cf7a1f81c4dccc917af7fcff3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-29 23:43:04 -0300

    Creating authController and .env files

---
+++ src/app/controllers/AuthController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import bcrypt from "bcryptjs";
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+class UserController {
+  async authenticate(req: Request, res: Response) {
+    const repository = getRepository(User);
+    const { email, password } = req.body;
+
+    const user = await repository.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401);
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.sendStatus(401);
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return res.json({
+      user,
+      token,
+    });
+  }
+}
+
+export default new UserController();
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,6 +1,12 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
-
+import bcrypt from "bcryptjs";
 @Entity("users")
 class User {
   @PrimaryGeneratedColumn("uuid")
@@ -11,6 +17,12 @@ class User {
 
   @Column()
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 
   constructor() {
     if (!this.id) {
